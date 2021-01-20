7917d371263556c8fd64654272f97e4b02f3260d
sahinkarakulak <karakulaksahin@gmail.com>
2021-01-20 23:40:24 +0300

    .

--- app/src/main/java/com/mrcaracal/mobilgezirehberim/A_Harita.java
+++ app/src/main/java/com/mrcaracal/mobilgezirehberim/A_Harita.java
@@ -1,9 +1,9 @@
 package com.mrcaracal.mobilgezirehberim;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
@@ -12,6 +12,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,25 +22,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class A_Maps extends AppCompatActivity implements OnMapReadyCallback {
+public class A_Harita extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
     LocationManager locationManager;
     LocationListener locationListener;
 
-    double enlem;
-    double boylam;
+    double enlem, boylam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_harita);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -52,34 +52,34 @@ public class A_Maps extends AppCompatActivity implements OnMapReadyCallback {
                 enlem = location.getLatitude();
                 boylam = location.getLongitude();
 
-                Toast.makeText(A_Maps.this, "Enlem: "+enlem+"\nBoylam: "+boylam, Toast.LENGTH_SHORT).show();
-                konumaGit();
+                Toast.makeText(getApplicationContext(), "Enlem: "+enlem+"\nBoylam: "+boylam, Toast.LENGTH_SHORT).show();
+                konumuBul();
+
             }
         };
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},3);
+            // İzin işlemleri
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 4);
         }else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5,5,locationListener);
+            // Lokasyon işlemleri
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 3, locationListener);
         }
-
-
-
     }
 
-    private void konumaGit(){
-        LatLng secilenKonum = new LatLng(enlem, boylam);
-        mMap.addMarker(new MarkerOptions().position(secilenKonum).title("Konumum"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(secilenKonum,15));
+    private void konumuBul(){
+        LatLng konum = new LatLng(enlem, boylam);
+        mMap.addMarker(new MarkerOptions().position(konum).title("KONUM"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(konum, 15));
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
         if (grantResults.length > 0){
-            if (requestCode == 3){
+            if (requestCode == 4){
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5,5,locationListener);
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 3, locationListener);
                 }
             }
         }
