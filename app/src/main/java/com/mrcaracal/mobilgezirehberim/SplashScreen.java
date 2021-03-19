7e3af64b0ebe83307cf11cec35929ae3d8d67873
sahinkarakulak <karakulaksahin@gmail.com>
2021-03-19 18:28:35 +0300

    .

--- app/src/main/java/com/mrcaracal/mobilgezirehberim/SplashScreen.java
+++ app/src/main/java/com/mrcaracal/mobilgezirehberim/SplashScreen.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView txt_giris_yazisi;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        txt_giris_yazisi = findViewById(R.id.txt_giris_yazisi);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim1);
+        txt_giris_yazisi.startAnimation(animation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
