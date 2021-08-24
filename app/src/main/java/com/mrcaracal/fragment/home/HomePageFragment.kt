2dd3f4b3a934ea8b253ec3c2c76f484e714b7fec
sahinkarakulak <38869245+sahinkarakulak@users.noreply.github.com>
2021-08-24 14:09:40 +0300

    create viewmodel for HomePage Step:1

--- app/src/main/java/com/mrcaracal/fragment/home/HomePageFragment.kt
+++ app/src/main/java/com/mrcaracal/fragment/home/HomePageFragment.kt
@@ -16,16 +16,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.mrcaracal.Interface.RecyclerViewClickInterface
 import com.mrcaracal.activity.GoToLocationOnMapActivity
 import com.mrcaracal.adapter.RecyclerAdapterStructure
 import com.mrcaracal.extensions.toast
 import com.mrcaracal.fragment.model.PostModel
+import com.mrcaracal.fragment.model.PostModelProvider
 import com.mrcaracal.mobilgezirehberim.R
 import com.mrcaracal.mobilgezirehberim.databinding.FragHomePageBinding
+import com.mrcaracal.modul.Posts
 import com.mrcaracal.modul.UserAccountStore
 import java.text.DateFormat
+import java.util.ArrayList
 
 class HomePageFragment : Fragment(), RecyclerViewClickInterface {
 
@@ -42,7 +48,11 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
     var latitude = 0.0
     var longitude = 0.0
 
-    private lateinit var firebaseOperationForHome: FirebaseOperationForHome
+    private val COLLECTION_NAME_SAVED = "Kaydedilenler"
+    private val COLLECTION_NAME_THEY_SAVED = "Kaydedenler"
+    private val COLLECTION_NAME_POST = "Gonderiler"
+
+    val postModelsList: ArrayList<PostModel> = arrayListOf()
 
     private fun init() {
         firebaseAuth = FirebaseAuth.getInstance()
@@ -51,9 +61,6 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
 
         GET = activity!!.getSharedPreferences(getString(R.string.map_key), Context.MODE_PRIVATE)
         SET = GET.edit()
-
-        firebaseOperationForHome = FirebaseOperationForHome()
-
     }
 
     override fun onCreateView(
@@ -66,15 +73,81 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
         val view = binding.root
 
         binding.recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerAdapterStructure = RecyclerAdapterStructure(
-            this
-        )
+        recyclerAdapterStructure = RecyclerAdapterStructure(this)
         binding.recyclerView.adapter = recyclerAdapterStructure
-
-        // Yeniden eskiye çekme
-        firebaseOperationForHome.rewind(FirebaseFirestore.getInstance(), recyclerAdapterStructure)
+        rewind()
 
         return view
+    }
+
+    fun rewind() {
+        val collectionReference = firebaseFirestore
+            .collection(COLLECTION_NAME_POST)
+
+        // VT'ye kaydedilme zamanına göre verileri çek
+        collectionReference
+            .orderBy("zaman", Query.Direction.DESCENDING)
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val querySnapshot = (task.result)
+                    for (snapshot: DocumentSnapshot in querySnapshot) {
+
+                        snapshot.data?.let {
+                            val postModel = PostModelProvider.provide(it)
+                            postModelsList.add(postModel)
+                        }
+                        recyclerAdapterStructure.postModelList = postModelsList
+                        recyclerAdapterStructure.notifyDataSetChanged()
+
+                    }
+                }
+            }
+    }
+
+    fun saveOperations(postModel: PostModel) {
+        if ((postModel.userEmail == firebaseUser!!.email)) {
+            //activity?.let { toast(it, "Bunu zaten siz paylaştınız") }
+        } else {
+            val MGonderiler = Posts(
+                postModel.postId,
+                postModel.userEmail,
+                postModel.pictureLink,
+                postModel.placeName,
+                postModel.location,
+                postModel.address,
+                postModel.city,
+                postModel.comment,
+                postModel.postCode,
+                listOf(postModel.tag),
+                FieldValue.serverTimestamp()
+            )
+            val documentReference = firebaseFirestore
+                .collection(COLLECTION_NAME_THEY_SAVED)
+                .document((firebaseUser!!.email)!!)
+                .collection(COLLECTION_NAME_SAVED)
+                .document(postModel.postId)
+            documentReference
+                .set(MGonderiler)
+                .addOnSuccessListener {
+                    //
+                }
+                .addOnFailureListener { e ->
+                    //
+                }
+        }
+    }
+
+    fun showTag(postModel: PostModel): String {
+        var taggg = ""
+        val al_taglar = postModel.tag
+        val tag_uzunluk = al_taglar.length
+        val alinan_taglar = al_taglar.substring(1, tag_uzunluk - 1)
+        val a_t = alinan_taglar.split(",").toTypedArray()
+        for (tags: String in a_t) {
+            taggg += "#" + tags.trim { it <= ' ' } + " "
+        }
+        return taggg
     }
 
     fun goToLocationOperations(postModel: PostModel) {
@@ -114,7 +187,7 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
         sharing.text = postModel.userEmail
         date.text = dateAndTime
         addres.text = postModel.address
-        labels.text = firebaseOperationForHome.showTag(postModel)
+        labels.text = showTag(postModel)
 
         val mAlertDialog = mBuilder.create()
         mDialogView.findViewById<Button>(R.id.dw_ok).setOnClickListener {
@@ -143,9 +216,8 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
         bottomSheetView.findViewById<View>(R.id.bs_postSave).setOnClickListener(
             View.OnClickListener {
                 firebaseUser?.let { it1 ->
-                    firebaseOperationForHome.saveOperations(
-                        postModel,
-                        it1, firebaseFirestore
+                    saveOperations(
+                        postModel
                     )
                 }
                 bottomSheetDialog.dismiss()
@@ -199,5 +271,4 @@ class HomePageFragment : Fragment(), RecyclerViewClickInterface {
         super.onDestroyView()
         _binding = null
     }
-
 }
