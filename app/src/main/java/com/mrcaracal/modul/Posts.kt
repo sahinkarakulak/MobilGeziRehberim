a6ee1ba6504f1106429f41ca7470ab12d2d9a95b
sahinkarakulak <38869245+sahinkarakulak@users.noreply.github.com>
2021-08-31 13:22:06 +0300

    Posts and UserInfo renewed

--- app/src/main/java/com/mrcaracal/modul/Posts.kt
+++ app/src/main/java/com/mrcaracal/modul/Posts.kt
@@ -2,42 +2,16 @@ package com.mrcaracal.modul
 
 import com.google.firebase.firestore.FieldValue
 
-class Posts {
-    var gonderiID: String? = null
-    var kullaniciEposta: String? = null
-    var resimAdresi: String? = null
-    var yerIsmi: String? = null
-    var konum: String? = null
-    var adres: String? = null
-    var sehir: String? = null
-    var yorum: String? = null
-    var postaKodu: String? = null
-    var taglar: List<String>? = null
-    var zaman: FieldValue? = null
-
-    constructor(
-        gonderiID: String?,
-        kullaniciEposta: String?,
-        resimAdresi: String?,
-        yerIsmi: String?,
-        konum: String?,
-        adres: String?,
-        sehir: String?,
-        yorum: String?,
-        postaKodu: String?,
-        taglar: List<String>?,
-        zaman: FieldValue?
-    ) {
-        this.gonderiID = gonderiID
-        this.kullaniciEposta = kullaniciEposta
-        this.resimAdresi = resimAdresi
-        this.yerIsmi = yerIsmi
-        this.konum = konum
-        this.adres = adres
-        this.sehir = sehir
-        this.yorum = yorum
-        this.postaKodu = postaKodu
-        this.taglar = taglar
-        this.zaman = zaman
-    }
-}
+class Posts(
+    val gonderiID: String? = null,
+    val kullaniciEposta: String? = null,
+    val resimAdresi: String? = null,
+    val yerIsmi: String? = null,
+    val konum: String? = null,
+    val adres: String? = null,
+    val sehir: String? = null,
+    val yorum: String? = null,
+    val postaKodu: String? = null,
+    val taglar: List<String>? = null,
+    val zaman: FieldValue? = null
+)
--- app/src/main/java/com/mrcaracal/modul/UserInfo.kt
+++ app/src/main/java/com/mrcaracal/modul/UserInfo.kt
@@ -1,23 +1,9 @@
 package com.mrcaracal.modul
 
-class UserInfo {
-    var kullaniciAdi: String? = null
-    var kullaniciEposta: String? = null
-    var kulaniciParola: String? = null
-    var bio: String? = null
-    var kullaniciResmi: String? = null
-
-    constructor(
-        kullaniciAdi: String?,
-        kullaniciEposta: String?,
-        kulaniciParola: String?,
-        bio: String?,
-        kullaniciResmi: String?
-    ) {
-        this.kullaniciAdi = kullaniciAdi
-        this.kullaniciEposta = kullaniciEposta
-        this.kulaniciParola = kulaniciParola
-        this.bio = bio
-        this.kullaniciResmi = kullaniciResmi
-    }
-}
+class UserInfo(
+    val kullaniciAdi: String? = null,
+    val kullaniciEposta: String? = null,
+    val kulaniciParola: String? = null,
+    val bio: String? = null,
+    val kullaniciResmi: String? = null
+)
