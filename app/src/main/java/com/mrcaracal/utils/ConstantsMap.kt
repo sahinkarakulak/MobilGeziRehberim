34adada1577e7c6c710a2e0babc86e39c42fe996
sahinkarakulak <38869245+sahinkarakulak@users.noreply.github.com>
2021-08-31 13:08:35 +0300

    Create ConstantsMap

--- app/src/main/java/com/mrcaracal/utils/ConstantsMap.kt
+++ app/src/main/java/com/mrcaracal/utils/ConstantsMap.kt
@@ -1,6 +1,7 @@
 package com.mrcaracal.utils
 
 object ConstantsMap {
+
     const val GO_TO_LOCATION_LATITUDE = "go_to_location_latitude"
     const val GO_TO_LOCATION_LONGITUDE = "go_to_location_longitude"
     const val POST_LOCATION = "Gönderi Konumu"
