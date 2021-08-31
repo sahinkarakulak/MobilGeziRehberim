d09998afc0b83938bcf71cfe3249ed8a0ca65fe5
sahinkarakulak <38869245+sahinkarakulak@users.noreply.github.com>
2021-08-31 11:23:01 +0300

    Create ConstantsFirebase

---
+++ app/src/main/java/com/mrcaracal/utils/ConstantsMap.kt
@@ -0,0 +1,12 @@
+package com.mrcaracal.utils
+
+object ConstantsMap {
+    const val GO_TO_LOCATION_LATITUDE = "go_to_location_latitude"
+    const val GO_TO_LOCATION_LONGITUDE = "go_to_location_longitude"
+    const val POST_LOCATION = "Gönderi Konumu"
+
+    const val LATITUDE = "enlem"
+    const val LONGITUDE = "boylam"
+    const val ADDRES = "adres"
+    const val POST_CODE = "postaKodu"
+}
